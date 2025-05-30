806bb588a8f56dafc1a9881ff2a42bc1f8df2a93
Gairus Niel <nielstupidity@gmail.com>
2025-05-31 02:33:51 +0800

    added basic nodes for placeholder

---
+++ Scenes/menu.gd
@@ -0,0 +1,74 @@
+extends Control
+
+@onready var tab_buttons : HBoxContainer = $TabButtons
+@onready var tab_buttons_overlay : HBoxContainer = $TabButtonsOverlay
+@onready var tab_buttons_highlight: Node = $TabButtonsOverlay/HighlightMark
+@onready var pages_container : Control = $TabPages
+
+var current_page = 0
+var tween : Tween = null
+var swipe_start_pos : Vector2
+var swipe_threshold : float = 100.0 
+
+
+func _ready():
+	# Hook up tab button signals
+	for i in range(tab_buttons.get_child_count()):
+		var button = tab_buttons.get_child(i)
+		button.pressed.connect(_on_tab_button_pressed.bind(i))
+	
+	# Show only the first page at start
+	for i in range(pages_container.get_child_count()):
+		var page = pages_container.get_child(i)
+		page.visible = (i == current_page)
+	
+	_on_tab_button_pressed(0)
+
+
+func _on_tab_button_pressed(target_index):
+	tab_buttons.get_child(current_page).size_flags_stretch_ratio = 1.0
+	tab_buttons.get_child(target_index).size_flags_stretch_ratio = 1.5
+	tab_buttons_overlay.move_child(tab_buttons_highlight, target_index)
+	
+	if target_index == current_page:
+		return
+	
+	var direction = 1 if target_index > current_page else -1
+	
+	var old_page = pages_container.get_child(current_page)
+	var new_page = pages_container.get_child(target_index)
+	
+	new_page.visible = true
+	new_page.position.x = direction * pages_container.size.x
+	old_page.position.x = 0
+	
+	if tween and tween.is_running():
+		tween.kill()
+	
+	tween = create_tween()
+	tween.tween_property(old_page, "position:x", -direction * pages_container.size.x, 0.05).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	tween.tween_property(new_page, "position:x", 0, 0.05).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	tween.tween_callback(Callable(self, "_on_tab_animation_finished").bind(old_page))
+	current_page = target_index
+
+
+func _on_tab_animation_finished(old_page):
+	old_page.visible = false
+	old_page.position.x = 0 
+
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		swipe_start_pos = event.position
+	
+	elif event is InputEventScreenDrag:
+		# later use for the process to animate swipe preview
+		pass
+	
+	elif event is InputEventScreenTouch and not event.pressed:
+		var delta = event.position - swipe_start_pos
+		if abs(delta.x) > swipe_threshold:
+			if delta.x < 0 and current_page < pages_container.get_child_count() - 1:
+				_on_tab_button_pressed(current_page + 1)  # Swipe left
+			elif delta.x > 0 and current_page > 0:
+				_on_tab_button_pressed(current_page - 1)  # Swipe right
