bc737753eedfa7136c7a27f2d7084264e58fd916
Gairus Niel <nielstupidity@gmail.com>
2025-06-05 03:38:26 +0800

    fixed the blocking of contents by the tab buttons

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -9,10 +9,11 @@ const DEFAULT_TAB_RATIO : float = 1.0
 const HIGHLIGHTED_TAB_RATIO : float = 1.5
 const DIRECTION_BIAS : int = 5
 const DIRECTION_LOCK_THRESHOLD : int = 10
-@onready var tab_buttons : HBoxContainer = $TabButtons
-@onready var tab_buttons_overlay : HBoxContainer = $TabButtonsOverlay
-@onready var tab_buttons_highlight: Node = $TabButtonsOverlay/HighlightMark
-@onready var pages_container : Control = $TabPages
+@onready var tab_buttons : HBoxContainer = $MainContainer/TabButtonsContainer/TabButtons
+@onready var tab_buttons_overlay : HBoxContainer = $MainContainer/TabButtonsContainer/TabButtonsOverlay
+@onready var tab_buttons_highlight: Node = $MainContainer/TabButtonsContainer/TabButtonsOverlay/HighlightMark
+@onready var tab_pages : Control = $MainContainer/TabPages
+@onready var tab_buttons_container : PanelContainer = $MainContainer/TabButtonsContainer
 
 var swipe_start_pos := Vector2.ZERO
 var swipe_direction_locked : String = EMPTY
@@ -28,12 +29,13 @@ var swipe_threshold : float
 func _ready():
 	var page_width = get_viewport_rect().size.x
 	var page_height = get_viewport_rect().size.y
-	var page_count = pages_container.get_child_count()
-	pages_container.size = Vector2(page_width * page_count, page_height)
+	var page_count = tab_pages.get_child_count()
+	$MainContainer.size = Vector2(page_width * page_count, page_height)
+	tab_buttons_container.custom_minimum_size = Vector2(page_width, 100.0)
 	swipe_threshold = get_viewport_rect().size.x * 0.5
 	
 	for i in range(page_count):
-		var page = pages_container.get_child(i)
+		var page = tab_pages.get_child(i)
 		page.size = Vector2(page_width, page_height)
 		page.position = Vector2(i * page_width, 0)
 	
@@ -46,7 +48,7 @@ func _ready():
 
 
 func _input(event: InputEvent) -> void:
-	current_scroll_container = pages_container.get_child(current_page_index).get_node("ScrollContainer")
+	current_scroll_container = tab_pages.get_child(current_page_index).get_node("ScrollContainer")
 	
 	if event is InputEventScreenTouch:
 		if event.pressed:
@@ -63,7 +65,7 @@ func _input(event: InputEvent) -> void:
 				next_page_index = current_page_index
 				
 				if abs(swipe_amount) > swipe_threshold:
-					if swipe_amount < 0 and current_page_index < pages_container.get_child_count() - 1:
+					if swipe_amount < 0 and current_page_index < tab_pages.get_child_count() - 1:
 						next_page_index = current_page_index + 1
 					elif swipe_amount > 0 and current_page_index > 0:
 						next_page_index = current_page_index - 1
@@ -83,12 +85,12 @@ func _input(event: InputEvent) -> void:
 		if swipe_direction_locked == HORIZONTAL:
 			var offset = -current_page_index * page_width + delta.x
 			
-			# Clamp when swiping beyond first and last pages
-			var min_offset = - ((pages_container.get_child_count() - 1) * page_width) - 50.0
-			var max_offset = 50.0
-			offset = clamp(offset, min_offset, max_offset)
+			## Clamp when swiping beyond first and last pages
+			#var min_offset = - ((tab_pages.get_child_count() - 1) * page_width) - 50.0
+			#var max_offset = 50.0
+			#offset = clamp(offset, min_offset, max_offset)
 			
-			pages_container.position.x = offset
+			tab_pages.position.x = offset
 			current_scroll_container.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		else:
 			current_scroll_container.mouse_filter = Control.MOUSE_FILTER_PASS
@@ -103,7 +105,7 @@ func snap_to_page(page_index: int) -> void:
 		tween.kill()
 	
 	tween = get_tree().create_tween()
-	tween.tween_property(pages_container, "position:x", target_x, 0.25).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(tab_pages, "position:x", target_x, 0.25).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
 	current_page_index = page_index
 
 
